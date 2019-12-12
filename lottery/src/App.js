a64e65505fb67b406d339a41c0a873d8dffd306d
Effanuel <32594043+Effanuel@users.noreply.github.com>
2019-12-12 02:26:03 +0200

    More redux functions added to interface

--- lottery/src/App.js
+++ lottery/src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { connect } from "react-redux";
-import { getManager, getPlayers } from "./redux/actions/lotteryActions";
+import {
+  getManager,
+  getPlayers,
+  getWinner
+} from "./redux/actions/lotteryActions";
 
 import web3 from "./web3";
 
@@ -9,16 +13,35 @@ import lottery from "./lottery";
 
 const initialState = {
   value: "",
-  message: ""
+  message: "",
+  currentAccount: ""
 };
 class App extends React.Component {
   state = initialState;
 
   async componentDidMount() {
-    this.props.getManager();
+    await this.props.getManager();
     this.props.getPlayers();
+    window.ethereum.on("accountsChanged", this.handleAccountChange);
+
     // const balance = await getBalance();
   }
+  static getDerivedStateFromProps(nextProps, prevState) {
+    return nextProps.manager.toLowerCase() === prevState.currentAccount
+      ? { currentAccount: "Lottery Manager" }
+      : {};
+  }
+
+  handleAccountChange = accounts => {
+    const currentAccount = accounts[0];
+    if (this.state.currentAccount !== currentAccount) {
+      if (currentAccount === this.props.manager.toLowerCase()) {
+        this.setState({ currentAccount: "Lottery Manager" });
+      } else {
+        this.setState({ currentAccount });
+      }
+    }
+  };
 
   handleChange = event => {
     this.setState({ [event.target.id]: event.target.value });
@@ -29,32 +52,25 @@ class App extends React.Component {
     const accounts = await web3.eth.getAccounts();
 
     this.setState({ message: "Waitin..." });
+    console.log(accounts);
     await lottery.methods.enter().send({
       from: accounts[0],
-      value: web3.utils.toWei(this.state.value, "ether")
+      value: web3.utils.toWei("0.011", "ether")
     });
-
+    console.log("entered");
     this.setState({ message: "Entered" });
   };
 
   handlePickWinner = async () => {
-    const accounts = await web3.eth.getAccounts();
-
-    this.setState({ message: "Waitin..." });
-
-    await lottery.methods.winner().send({
-      from: accounts[0]
-    });
-
-    this.setState({ message: "A winner is picked." });
+    this.props.getWinner();
   };
   render() {
-    const { players, value, message } = this.state;
-    const { manager, playersLength, balance } = this.props;
+    const { players, value, currentAccount } = this.state;
+    const { playersLength, balance, message, error } = this.props;
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p>This contract is managed by {manager}.</p>
+        <h3>Current account is {currentAccount}</h3>
         <p>There are currently {playersLength} people entered, competing to</p>
         <p>win {web3.utils.fromWei(balance, "ether")} ether!</p>
         <hr />
@@ -62,22 +78,21 @@ class App extends React.Component {
         <form onSubmit={this.handleEnter}>
           <h4>Try your luck!</h4>
           <div>
-            <label>Enter Amount to enter</label>
-            <input
-              id="value"
-              // type="number"
-              // pattern="*[0-9]"
-              value={value}
-              onChange={this.handleChange}
-            ></input>
+            <label>Buy a ticket for 0.011 eth</label>
           </div>
           <button>Enter</button>
         </form>
         <hr />
-        <h4> Pick a winner</h4>
-        <button onClick={this.handlePickWinner}>Winner</button>
-        <hr />
+        {currentAccount === "Lottery Manager" && (
+          <div>
+            <h4> Pick a winner</h4>
+            <button onClick={this.handlePickWinner}>Winner</button>
+            <hr />
+          </div>
+        )}
+
         <h1>{message}</h1>
+        <h1>{error}</h1>
       </div>
     );
   }
@@ -87,7 +102,11 @@ const mapStateToProps = state => ({
   loading: state.lottery.loading,
   manager: state.lottery.manager,
   playersLength: state.lottery.players.length,
-  balance: state.lottery.balance
+  balance: state.lottery.balance,
+  message: state.lottery.message,
+  error: state.lottery.error
 });
 
-export default connect(mapStateToProps, { getManager, getPlayers })(App);
+export default connect(mapStateToProps, { getManager, getPlayers, getWinner })(
+  App
+);
